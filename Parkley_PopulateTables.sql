8b10278c9afe23bbe85ae06b21cc6b11a74fb1f9
Ian Figon <ifigon@outlook.com>
2018-05-09 20:55:32 -0700

    updates

--- Parkley_PopulateTables.sql
+++ Parkley_PopulateTables.sql
@@ -136,7 +136,7 @@ WHILE @RUN > 0
 		END
 	
 	SET @UserID = ROUND(@UserID, 0)
-	SET @TotalPrice = CAST((RAND() * 40) AS decimal(6,2))
+	SET @TotalPrice = CAST(0.0 AS decimal(6,2))
 	INSERT INTO RENTAL (UserID, TotalPrice)
 	VALUES (@UserID, @TotalPrice)
 	
@@ -145,9 +145,99 @@ WHILE @RUN > 0
 	END
 
 	
-SELECT * FROM RENTAL
+SELECT * FROM RATING
 EXEC uspInsertDataIntoRental @RUN=94000
 
 
+CREATE PROCEDURE uspInsertDataIntoRating
+AS
+
+DECLARE @RatingLevel decimal(2,1)
+DECLARE @RatingName varchar(20)
+
+SET @RatingLevel = CAST(1.0 AS DECIMAL(2,1))
+SET @RatingName = 'Terrible'
+
+WHILE @RatingLevel <= 5.0
+	BEGIN
+	
+	IF @RatingLevel BETWEEN 2.0 AND 2.9
+		BEGIN
+		SET @RatingName = 'Poor'
+		END
+	
+	IF @RatingLevel BETWEEN 3.0 AND 3.9
+		BEGIN
+		SET @RatingName = 'Average'
+		END	
+	
+	IF @RatingLevel BETWEEN 4.0 AND 4.9
+		BEGIN
+		SET @RatingName = 'Great'
+		END	
+		
+	IF @RatingLevel = 5.0
+		BEGIN
+		SET @RatingName = 'Outstanding'
+		END	
 
 
+	INSERT INTO RATING (RatingLevel, RatingName)
+	VALUES (@RatingLevel, @RatingName)
+	
+	SET @RatingLevel = @RatingLevel + 0.1
+	
+	END
+
+EXEC uspInsertDataIntoRating
+
+SELECT * FROM RATING
+
+
+CREATE PROCEDURE uspInsertDataIntoReview
+@RUN INT
+AS
+
+DECLARE @ReviewTitle varchar(30) = 'Lorem ipsum'
+DECLARE @ReviewBody varchar(4000) = 'Lorem ipsum dolor sit amet, eam ex wisi volumus, ut decore essent vel. Te eum omnis senserit constituam, est ne verterem mediocrem. Duo docendi persecuti in. His euripidis assueverit ne. Illud moderatius cum no, nec dolores salutandi qualisque et, no cum suscipit antiopam scripserit.'
+
+DECLARE @RatingNumber INT = (SELECT COUNT(*) FROM RATING)
+DECLARE @RentalNumber INT = (SELECT COUNT(*) FROM RENTAL)
+
+DECLARE @RatingID INT
+DECLARE @RentalID INT
+
+WHILE @RUN > 0
+	BEGIN
+	SET @RatingID = (SELECT RAND() * @RatingNumber)
+	IF @RatingID <1
+		BEGIN
+		SET @RatingID = (SELECT RAND() * @RatingNumber)
+		END
+		IF @RatingID <1
+			BEGIN
+			SET @RatingID = (SELECT RAND() * @RatingNumber)
+			END
+		
+	SET @RentalID = (SELECT RAND() * @RentalNumber)
+	IF @RentalID <1
+		BEGIN
+		SET @RentalID = (SELECT RAND() * @RentalNumber)
+		END
+	
+	SET @RatingID = ROUND(@RatingID, 0)
+	SET @RentalID = ROUND(@RentalID, 0)
+	
+	INSERT INTO REVIEW (ReviewTitle, ReviewBody, RentalID, RatingID)
+	VALUES (@ReviewTitle, @ReviewBody, @RentalID, @RatingID)
+	
+	SET @RUN = @RUN - 1
+	
+	END
+
+EXEC uspInsertDataIntoReview @RUN = 100000
+
+
+DROP TABLE TIMESPOT
+SELECT * FROM TIMESPOT
+SELECT * FROM LOCATION
