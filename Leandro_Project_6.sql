c755babec54d4cf038c15a1298cc15f1ef4c0700
Leandro Solidum <lndrgsol@gmail.com>
2018-05-27 17:40:04 -0700

    update

--- Leandro_Project_6.sql
+++ Leandro_Project_6.sql
@@ -39,3 +39,22 @@ GO
 ALTER TABLE [OWNER]
 	ADD OwnerAvgRating AS dbo.fnCalculateAvgOwnerRating(OwnerID)
 GO
+
+-- View 1: Users who incurred a fee for overtime on a parallel parking spot in 2018
+SELECT U.UserFname, U.UserLname, DT.DateTimeStart, DT.DateTimeEnd
+FROM RENTAL R
+	JOIN [USER] U ON U.UserID = R.UserID
+	JOIN TIMESPOT TS ON TS.RentalID = R.RentalID
+	JOIN [DATETIME] DT ON DT.DateTimeID = TS.DateTimeID
+	JOIN SPOT S ON S.SpotID = TS.SpotID
+	JOIN SPOT_TYPE ST ON ST.SpotTypeID = S.SpotTypeID
+WHERE R.RentalID IN (
+	SELECT RF.RentalID 
+	FROM RENTAL_FEE RF
+		JOIN FEE F ON F.FeeID = RF.FeeID
+	WHERE F.FeeName = 'Overtime Violation')
+AND ST.SpotTypeName = 'Parallel'
+AND DT.DateTimeStart >= '01/01/2018'
+GO
+
+-- View 2:
